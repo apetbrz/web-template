efafc2480aa7c171dc0841b566e067fd52755bea
Arthur Petroff <apetbrz@gmail.com>
2025-09-12 15:41:10 -0400

    fixed rename script not working

--- rename.sh
+++ rename.sh
@@ -3,4 +3,4 @@ FILES= rg -l -g "!rename.sh" "CHANGEME" .
 echo "Renaming project to ${1}"
 echo $FILES
 
-echo $FILES | xargs sed -i "s/CHANGEME/${1}/g" && echo "Success! Cleaning up..." && rm -v rename.sh || echo "Failure?"
+rg -l -g "!rename.sh" "CHANGEME" . | xargs sed -i "s/CHANGEME/${1}/g" && echo "Success! Cleaning up..." && rm -rvf rename.sh .git || echo "Failure"
